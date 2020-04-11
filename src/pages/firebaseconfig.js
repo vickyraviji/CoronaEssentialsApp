9c81d963c4f18f526387e29628abb6f5d976171b
premmunnaa <premmano98@gmail.com>
2020-04-11 12:16:44 +0530

    prem commmit

---
+++ src/pages/firebaseconfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBfy5nYbmlMsQpK_dbwsH1s83cxTdA5EGY",
+    authDomain: "corona-6c229.firebaseapp.com",
+    databaseURL: "https://corona-6c229.firebaseio.com",
+    projectId: "corona-6c229",
+    storageBucket: "corona-6c229.appspot.com",
+    messagingSenderId: "919465148200",
+    appId: "1:919465148200:web:5766c4bb4c232162569a1d",
+    measurementId: "G-MYVYKS4VLT"
+  };
+
+  export default firebaseConfig;
